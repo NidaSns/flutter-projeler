4e5556dabf4d0be6e490d5c21fed79be53ff63b1
Nida <112012287+NidaSns@users.noreply.github.com>
2022-12-09 23:22:56 +0300

    Create odev2_main.dart

---
+++ techcareer_flutter_odevler/odev2_main.dart
@@ -0,0 +1,12 @@
+import 'odev2.dart';
+
+void main() {
+  var o2 = Odev2();
+  print("Fahrenheit: ${o2.dereceToFahrenhiet(30)}");
+  print("Sayının faktöriyel değeri: ${o2.faktoriyel(5)}");
+  print("Diktörtgen'in çevresi: ${o2.diktortgenCevresiBulma(5, 10)}");
+  print("a harfi sayısı: ${o2.aHarfiSayisi('nidavebatuhan')}");
+  print("bir kenarın açısı: ${o2.icAciOlcusu(4)}");
+  print("maaşı: ${o2.maasHesabi(20)}");
+  print("otopark ücreti: ${o2.otoparkUcreti(1)}");
+}
